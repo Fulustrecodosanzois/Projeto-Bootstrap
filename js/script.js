545e9069013cd2b4f23943243a37135530fffb65
Luciano Silva <luciano_jet@hotmail.com>
2023-08-07 21:11:29 -0300

    iniciado email

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ let nome = document.querySelector("#nome")
 
 function exibirNome() {
     console.log(nome.value)
-    if(nome.value= ""){
-        nome.style.border =  "2px solid red"
+    if (nome.value == "") {
+        nome.style.border = "2px solid red"
     }
-    else{
+    else {
         nome.style.border = "2px solid green"
     }
 }
@@ -20,3 +20,46 @@ nome.addEventListener("blur", exibirNome)// o addEventListener cria um evento pa
 // nome.addEventListener("focus", function(){
 //     nome.value = "Este campo não deve ficar vazio"
 // })
+
+let idade = document.querySelector("#idade")
+
+idade.addEventListener("blur", function () {
+    if (idade.value >= 18) {
+        alert("Você possui 18 anos ou mais")
+    }
+    else {
+        alert("Infelizmente você é menor de idade!")
+        window.location.href = "https://www.youtube.com/@PeppaPigPortuguesOficial/featured"
+    }
+})
+
+let endereco = document.querySelector("input[name=end]") /* pesquisa um seletor (input) que tenha o valor "name" com end*/
+endereco.addEventListener("blur", function () {
+    console.log(endereco.value.length)
+    if (endereco.value.length >= 35) {
+        alert("A quantidade de letras excede 35 letras")
+        endereco.style.border = "2px solid red"
+    }
+    else {
+        alert("O endereço esta menor")
+        endereco.style.border = "2px solid yellow"
+    }
+})
+
+let email = document.querySelector("#email")
+let conf = document.querySelector("#confirmar")
+
+let minusculo = email.value.toLowerCase() //torna o conteúdo minuscula
+
+email.addEventListener("keyup", function(){
+    conf.value = email.value
+    if(minusculo.indexOf("@") == -1 || minusculo.indexOf(".com") == -1){
+
+    }
+    else{
+
+    }
+    console.log(email.value.indexOf("@")) /*IndexOf retorna a posição do caracter de texto */
+
+})
+
